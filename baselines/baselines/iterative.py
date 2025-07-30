cab37ac4461f7e0d4dd086459a440fc55a7ef9c6
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2025-07-30 18:56:22 +0800

    feat: add dpo support.

--- baselines/baselines/iterative.py
+++ baselines/baselines/iterative.py
@@ -13,33 +13,32 @@ def unlearn(
     data_file: str,
     out_dir: str,
     retain_data_file: str | None = None,
-    loss_type: str = 'ga',
+    loss_type: str = "ga",
     per_device_batch_size: int = 2,
     epochs: int = 5,
     learning_rate=1e-5,
     max_len: int = 4096,
     tokenizer_dir: str | None = None,
-    resume_from_checkpoint: bool = False
+    resume_from_checkpoint: bool = False,
+    positive_data_file: str | None = None,
 ):
-    if 'gd' in loss_type:
-        assert retain_data_file is not None, "Retain data must be specified for grad_diff."
+    if "gd" in loss_type:
+        assert (
+            retain_data_file is not None
+        ), "Retain data must be specified for grad_diff."
 
-    model, tokenizer = load_model_and_tokenizer(
-        model_dir,
-        tokenizer_dir=tokenizer_dir
-    )
+    model, tokenizer = load_model_and_tokenizer(model_dir, tokenizer_dir=tokenizer_dir)
 
     ref_model = (
-        load_model(model_dir)
-        if 'npo' in loss_type or 'kl' in loss_type
-        else None
+        load_model(model_dir) if "npo" in loss_type or "kl" in loss_type else None
     )
 
     dataset = ForgetRetainDataset(
         data_file,
         tokenizer=tokenizer,
         retain_file_path=retain_data_file,
-        max_len=max_len
+        max_len=max_len,
+        positive_file_path=positive_data_file,
     )
 
     if device_count() == 0:
@@ -49,12 +48,12 @@ def unlearn(
         output_dir=out_dir,
         per_device_train_batch_size=per_device_batch_size,
         learning_rate=learning_rate,
-        save_strategy='epoch',  # Save every epoch
+        save_strategy="epoch",  # Save every epoch
         num_train_epochs=epochs,
-        optim='adamw_torch',
-        lr_scheduler_type='constant',
+        optim="adamw_torch",
+        lr_scheduler_type="constant",
         bf16=True,
-        report_to='none'        # Disable wandb
+        report_to="none",  # Disable wandb
     )
 
     trainer = IterativeUnlearner(
@@ -64,101 +63,155 @@ def unlearn(
         train_dataset=dataset,
         args=training_args,
         data_collator=dataset.get_collate_fn(),
-        loss_type=loss_type
+        loss_type=loss_type,
     )
     model.config.use_cache = False  # silence the warnings.
     trainer.train(resume_from_checkpoint=resume_from_checkpoint)
     trainer.save_model(out_dir)
 
 
-
 class IterativeUnlearner(Trainer):
-    """Source: https://github.com/locuslab/tofu/blob/main/dataloader.py
-    """
+    """Source: https://github.com/locuslab/tofu/blob/main/dataloader.py"""
 
-    def __init__(self, *args,
-                 loss_type: str = 'ga',
-                 ref_model: AutoModelForCausalLM | None = None,
-                 beta: float = 0.1,
-                 **kwargs):
+    def __init__(
+        self,
+        *args,
+        loss_type: str = "ga",
+        ref_model: AutoModelForCausalLM | None = None,
+        beta: float = 0.1,
+        **kwargs
+    ):
         self.loss_type = loss_type
         self.ref_model = ref_model
-        self.beta = beta    # Only relevant when `'po' in self.loss_type`
+        self.beta = beta  # Only relevant when `'po' in self.loss_type`
 
         if ref_model is not None:
-            assert 'po' in self.loss_type or 'kl' in self.loss_type
+            assert "po" in self.loss_type or "kl" in self.loss_type
             ref_model = ref_model.eval()
 
         super().__init__(*args, **kwargs)
 
-
     def compute_loss(self, model, x, return_outputs=False):
-        """Source: https://github.com/licong-lin/negative-preference-optimization/blob/main/synthetic/mymodel.py
-        """
-        
+        """Source: https://github.com/licong-lin/negative-preference-optimization/blob/main/synthetic/mymodel.py"""
+
         ### 1. Run model ###
-        x_f, x_r = x
+        x_f, x_r, x_p = x
         outputs_f = model(
-            x_f['input_ids'],
-            labels=x_f['labels'] if 'labels' in x_f else x_f['input_ids'].clone(),
-            attention_mask=x_f['attention_mask'] if 'attention_mask' in x_f else torch.ones_like(x_f['input_ids'], dtype=torch.bool)
+            x_f["input_ids"],
+            labels=x_f["labels"] if "labels" in x_f else x_f["input_ids"].clone(),
+            attention_mask=(
+                x_f["attention_mask"]
+                if "attention_mask" in x_f
+                else torch.ones_like(x_f["input_ids"], dtype=torch.bool)
+            ),
         )
         loss_f = outputs_f.loss
 
-        if 'gdr' in self.loss_type or 'klr' in self.loss_type:
+        if "gdr" in self.loss_type or "klr" in self.loss_type:
             outputs_r = model(
-                x_r['input_ids'],
-                labels=x_r['labels'] if 'labels' in x_r else x_r['input_ids'].clone(),
-                attention_mask=x_r['attention_mask'] if 'attention_mask' in x_r else torch.ones_like(x_r['input_ids'], dtype=torch.bool)
+                x_r["input_ids"],
+                labels=x_r["labels"] if "labels" in x_r else x_r["input_ids"].clone(),
+                attention_mask=(
+                    x_r["attention_mask"]
+                    if "attention_mask" in x_r
+                    else torch.ones_like(x_r["input_ids"], dtype=torch.bool)
+                ),
             )
             loss_r = outputs_r.loss
 
-        if 'klf' in self.loss_type or 'npo' in self.loss_type:
+        if (
+            "klf" in self.loss_type
+            or "npo" in self.loss_type
+            or "dpo" in self.loss_type
+        ):
             with torch.no_grad():
                 outputs_f_ref = self.ref_model(
-                    x_f['input_ids'],
-                    labels=x_f['labels'] if 'labels' in x_f else x_f['input_ids'].clone(),
-                    attention_mask=x_f['attention_mask'] if 'attention_mask' in x_f else torch.ones_like(x_f['input_ids'], dtype=torch.bool)
+                    x_f["input_ids"],
+                    labels=(
+                        x_f["labels"] if "labels" in x_f else x_f["input_ids"].clone()
+                    ),
+                    attention_mask=(
+                        x_f["attention_mask"]
+                        if "attention_mask" in x_f
+                        else torch.ones_like(x_f["input_ids"], dtype=torch.bool)
+                    ),
+                )
+        if "dpo" in self.loss_type:
+            with torch.no_grad():
+                outputs_p_ref = self.ref_model(
+                    x_p["input_ids"],
+                    labels=(
+                        x_p["labels"] if "labels" in x_p else x_p["input_ids"].clone()
+                    ),
+                    attention_mask=(
+                        x_p["attention_mask"]
+                        if "attention_mask" in x_p
+                        else torch.ones_like(x_p["input_ids"], dtype=torch.bool)
+                    ),
+                )
+                outputs_p = model(
+                    x_p["input_ids"],
+                    labels=(
+                        x_p["labels"] if "labels" in x_p else x_p["input_ids"].clone()
+                    ),
+                    attention_mask=(
+                        x_p["attention_mask"]
+                        if "attention_mask" in x_p
+                        else torch.ones_like(x_p["input_ids"], dtype=torch.bool)
+                    ),
                 )
 
-        if 'klr' in self.loss_type:
+        if "klr" in self.loss_type:
             with torch.no_grad():
                 outputs_r_ref = self.ref_model(
-                    x_r['input_ids'],
-                    labels=x_r['labels'] if 'labels' in x_r else x_r['input_ids'].clone(),
-                    attention_mask=x_r['attention_mask'] if 'attention_mask' in x_r else torch.ones_like(x_r['input_ids'], dtype=torch.bool)
+                    x_r["input_ids"],
+                    labels=(
+                        x_r["labels"] if "labels" in x_r else x_r["input_ids"].clone()
+                    ),
+                    attention_mask=(
+                        x_r["attention_mask"]
+                        if "attention_mask" in x_r
+                        else torch.ones_like(x_r["input_ids"], dtype=torch.bool)
+                    ),
                 )
 
         ### 2. Compute Loss ###
         loss = 0
 
-        if 'ga' in self.loss_type:
+        if "ga" in self.loss_type:
             loss += -loss_f
 
-        elif 'npo' in self.loss_type:
+        elif "npo" in self.loss_type:
             neg_log_ratio = outputs_f_ref.logits - outputs_f.logits
             loss += -F.logsigmoid(self.beta * neg_log_ratio).mean() * 2 / self.beta
+        elif "dpo" in self.loss_type:
+            neg_log_ratio = outputs_f_ref.logits - outputs_f.logits
+            pos_log_ratio = outputs_p.logits - outputs_p_ref.logits
+            loss += (
+                -F.logsigmoid(self.beta * (neg_log_ratio + pos_log_ratio)).mean()
+                * 2
+                / self.beta
+            )
 
         else:
             raise NotImplementedError("Cannot infer the given loss type.")
 
-        if 'gdr' in self.loss_type:
+        if "gdr" in self.loss_type:
             loss += loss_r
 
-        if 'klf' in self.loss_type:
+        if "klf" in self.loss_type:
             raise NotImplementedError("KL forget not implemented yet!")
 
-        if 'klr' in self.loss_type:
+        if "klr" in self.loss_type:
             kl_r = F.kl_div(
                 outputs_r.logits,
                 outputs_r_ref.logits,
-                reduction = 'batchmean',
-                log_target = True
+                reduction="batchmean",
+                log_target=True,
             )
             loss += kl_r
 
         return (loss, outputs_f) if return_outputs else loss
-
 
     def prediction_step(self, model, x, prediction_loss_only: bool, ignore_keys=None):
         input_ids, labels, attention_mask = x
